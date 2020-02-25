4c73c840ba9f18b712d3130c0c3d2def1b9566d6
Rammel Razon <rammel1012@gmail.com>
2020-02-25 15:30:40 -0800

    removed /all

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let path = require('path');
 
 app.use(express.static(path.join(__dirname,'/')))
 
-app.get('/all', (req, res) => {
+app.get('/', (req, res) => {
    //res.send("Hello")
    res.sendFile(path.join(__dirname+'/index.html'))
 });
